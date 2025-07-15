2784b4907214517ee49815a2fe11df7fe14ea797
Tito-21 <t.sibo@alustudent.com>
2025-07-15 15:09:36 +0200

    advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,23 +1,43 @@
 #!/usr/bin/python3
-"""documenting stuff"""
+"""Queries the Reddit API and
+prints the titles of the first
+10 hot posts listed for a given
+subreddit.
+"""
 import requests
 
-def top_ten(username):
-    url = f"https://api.example.com/users/{username}/top_ten"
-    try:
-        res = requests.get(url)
-    except requests.RequestException as e:
-        # You can print an error or handle it differently
-        return None
 
-    if res.status_code != 200:
-        return None
+def top_ten(subreddit):
+    """Prints the titles of the first
+    10 hot posts listed for a given
+    subreddit.
+    """
+    # Set the Default URL strings
+    base_url = 'https://www.reddit.com'
+    api_uri = '{base}/r/{subreddit}/hot.json'.format(base=base_url,
+                                                     subreddit=subreddit)
 
-    try:
+    # Set an User-Agent
+    user_agent = {'User-Agent': 'Python/requests'}
+
+    # Set the Query Strings to Request
+    payload = {'limit': '10'}
+
+    # Get the Response of the Reddit API
+    res = requests.get(api_uri, headers=user_agent,
+                       params=payload, allow_redirects=False)
+
+    # Checks if the subreddit is invalid
+    if res.status_code in [302, 404]:
+        print('None')
+    else:
         res_json = res.json()
-    except ValueError:
-        return None
 
-    # Process res_json as needed
+        if res_json.get('data') and res_json.get('data').get('children'):
+            # Get the 10 hot posts of the subreddit
+            hot_posts = res_json.get('data').get('children')
 
-    return "OK"
+            # Print each hot post title
+            for post in hot_posts:
+                if post.get('data') and post.get('data').get('title'):
+                    print(post.get('data').get('title'))
